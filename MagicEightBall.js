c7cf3c9234f46e4ba256df0f9ee59f638aa4268b
Kyle Matthew Rojas <55556079+Kyle-Matthew-Rojas@users.noreply.github.com>
2022-01-10 16:32:56 -0500

    Create MagicEightBall.js

---
+++ MagicEightBall.js
@@ -0,0 +1,43 @@
+let userName = "TestUser123";
+userName === "" ? console.log('Hello!') : console.log(`Hello, ${userName}!`);
+
+let userQuestion = "Will I win the lottery?";
+console.log(`${userName} asked: ${userQuestion}`);
+
+let randomNumber = Math.floor(Math.random() * 8);
+let eightBall = "";
+
+switch(randomNumber){
+  case 0:
+    eightBall = 'Probably not';
+    break;
+  case 1:
+    eightBall = 'It is certain';
+    break;
+  case 2:
+    eightBall = 'It is decidely so';
+    break;
+  case 3:
+    eightBall = 'Reply hazy try again';
+    break;
+  case 4:
+    eightBall = 'Cannot predict now';
+    break;
+  case 5:
+    eightBall = 'Do not count on it';
+    break;
+  case 6:
+    eightBall = 'My sources say no';
+    break;
+  case 7:
+    eightBall = 'Outlook not so good';
+    break;
+  case 8:
+    eightBall = 'Signs point to yes';
+    break;
+  default:
+    eightBall = 'Try Again';
+    break;
+}
+
+console.log(`The eight ball answered: ${eightBall}`);
