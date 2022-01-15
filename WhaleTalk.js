ace9154ff0281262db5920de7df465d3af6d57ae
Kyle Matthew Rojas <55556079+Kyle-Matthew-Rojas@users.noreply.github.com>
2022-01-14 23:08:01 -0500

    Create WhaleTalk.js

---
+++ WhaleTalk.js
@@ -0,0 +1,20 @@
+let input = "Hello, World!";
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+let resultArray = [];
+
+for(let i = 0; i < input.length; i++){
+  for(let j = 0; j < vowels.length; j++){
+    if(input[i] === vowels[j]){
+      resultArray.push(input[i]);
+         if(input[i] === 'e'){
+            resultArray.push(input[i]);
+      }
+    }
+  }
+};
+
+console.log(resultArray);
+
+let translatedIntoWhale = resultArray.join('');
+console.log(translatedIntoWhale.toUpperCase());
